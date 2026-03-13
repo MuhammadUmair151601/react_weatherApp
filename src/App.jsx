72fb0ba09cfa6d7abd3603f2d90a58bb1850eb38
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-03-13 15:01:18 +0500

    weather app structure ready

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div className='bg-[#0F172A] min-h-screen flex justify-center items-center'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
