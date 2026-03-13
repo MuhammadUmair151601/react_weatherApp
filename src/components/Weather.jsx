b6ff55ada259170783392f64f6f99121ede3d5e7
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-03-13 15:32:07 +0500

    UI and api calling all ready

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Weather = () => {
-  return (
-    <div>Weather</div>
-  )
-}
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
 
-export default Weather
+  const API_KEY = "c8c01d6a4aa8a1d625798c75254f9545";
+
+  const handleChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const fetchWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      setWeather(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log("Weather not found");
+      setWeather(null);
+    }
+  };
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-[#0F172A]">
+      <div className="bg-[#1E293B] border border-[#334155] h-120 w-100 text-blue-500 shadow-[0_20px_40px_rgba(0,0,0,0.6)] rounded-2xl p-6">
+        <h1 className="text-3xl font-bold flex justify-center mb-6">Weather Check</h1>
+        <div className="flex flex-col items-center gap-4">
+          <input
+            className="bg-gray-600 text-white px-5 py-2 rounded-[7px] w-72 outline-none"
+            type="text"
+            placeholder="Enter your city"
+            value={city}
+            onChange={handleChange}
+          />
+          <button
+            className="bg-blue-500 text-gray-200 text-lg font-semibold px-4 py-1 rounded-lg cursor-pointer active:scale-90 duration-300"
+            onClick={fetchWeather}
+          >
+            Get Weather
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
